c469fc17d288d8b089ac56609c4de26687849ca4
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2022-01-22 18:40:44 +0900

    favicon変更

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -5,7 +5,15 @@ export const Meta: React.VFC = () => {
     <Head>
       <title>経歴年度計算</title>
       <meta name='description' content='経歴年度を計算する' />
-      <link rel='icon' href='/favicon.ico' />
+      <link
+        rel='icon'
+        href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>📰</text></svg>'
+      />
+      <link
+        rel='icon alternate'
+        type='image/png'
+        href='https://twemoji.maxcdn.com/v/13.1.0/72x72/1f4f0.png'
+      />
     </Head>
   );
 };
